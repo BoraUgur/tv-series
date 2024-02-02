d4dc9b11e89a02db55a2ccdafcdce9c1b75df1b6
Bora Uğur <boraugur47@gmail.com>
2024-02-03 01:43:09 +0300

    first commit

---
+++ components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="page">
+        <div class="container">
+            
+        </div>
+    </div>
+</template>
+
+<style>
+    @import url("~/assets/css/homepage.css");
+</style>
